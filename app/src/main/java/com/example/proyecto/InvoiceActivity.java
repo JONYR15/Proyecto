34a8080ceafdc8362f3fc29e64b3d3f4b2abdc7b
Jonathan Ramirez <jonathan.ramirezz15@gmail.com>
2019-05-01 12:41:05 -0600

    .

--- app/src/main/java/com/example/proyecto/InvoiceActivity.java
+++ app/src/main/java/com/example/proyecto/InvoiceActivity.java
@@ -1,26 +1,22 @@
 package com.example.proyecto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-
 import com.example.proyecto.Adapter.InvoiceAdapter;
-import com.example.proyecto.Adapter.ProductAdapter;
 import com.example.proyecto.model.Invoice;
-import com.example.proyecto.model.Products;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,8 +41,9 @@ public class InvoiceActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), InvoiceManaged.class);
+                intent.putExtra("accion", 1);
+                view.getContext().startActivity(intent);
             }
         });
 

b4b15828579c2aac7ea629085675e779c966e1f6
Jonathan Ramirez <jonathan.ramirezz15@gmail.com>
2019-05-03 14:31:14 -0600

    .

--- app/src/main/java/com/example/proyecto/Managed/InvoiceManaged.java
+++ app/src/main/java/com/example/proyecto/Managed/InvoiceManaged.java
@@ -1,26 +1,20 @@
 package com.example.proyecto.Managed;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.proyecto.Activity.CustomersActivity;
-import com.example.proyecto.CustomersActivity;
-import com.example.proyecto.MainActivity;
 import com.example.proyecto.R;
 import com.example.proyecto.model.Customers;
 import com.example.proyecto.model.Invoice;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class InvoiceManaged extends AppCompatActivity {
 

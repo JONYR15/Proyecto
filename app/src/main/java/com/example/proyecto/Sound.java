a6254dc8494a0522314a11682314d26ce55db612
Bernan Ureña Lopez <bernan-1103@hotmail.com>
2019-05-03 18:22:05 -0600

    .

--- app/src/main/java/com/example/proyecto/Sound.java
+++ app/src/main/java/com/example/proyecto/Sound.java
@@ -3,14 +3,21 @@ package com.example.proyecto;
 import android.media.MediaPlayer;
 import android.view.View;
 
-public class SoundActivity {
+public class Sound {
 
     private MediaPlayer mp;
     int posicion = 0;
 
+    public Sound() {
+    }
 
-    public void Moneda() {
-        MediaPlayer mp = MediaPlayer.create(this,R.raw.mario_coin);
+    public void Moneda(View view) {
+        MediaPlayer mp = MediaPlayer.create(view.getContext(),R.raw.mario_coin);
+        mp.start();
+    }
+
+    public void Cliente(View view) {
+        MediaPlayer mp = MediaPlayer.create(view.getContext(),R.raw.);
         mp.start();
     }
 }
